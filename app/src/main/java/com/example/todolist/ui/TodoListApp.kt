2458d8b18807bf36bbe7f593ba9b6ca4caf7ebbd
Dudoladov Dmitry <dudmitry.iv@gmail.com>
2025-09-11 10:28:36 +0300

    feat: add top app bar with navigation

--- app/src/main/java/com/example/todolist/ui/TodoListApp.kt
+++ app/src/main/java/com/example/todolist/ui/TodoListApp.kt
@@ -1,18 +1,30 @@
 package com.example.todolist.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.todolist.R
 import com.example.todolist.ui.theme.TodoListTheme
 
 object TodoListPath {
@@ -20,6 +32,39 @@ object TodoListPath {
     const val CHANGE_SCREEN = "todo/{id}"
     const val CHANGE_SCREEN_BASE = "todo/"
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TodoListTopAppBar(
+    canNavigateHome: Boolean,
+    modifier: Modifier = Modifier,
+    navigateHome: () -> Unit = {},
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        },
+        navigationIcon = {
+            if (canNavigateHome) {
+                FilledTonalIconButton(
+                    onClick = { navigateHome() },
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Home,
+                        contentDescription = stringResource(R.string.back_top_btn)
+                    )
+                }
+            }
+        },
+        actions = {},
+        modifier = modifier
+    )
+}
+
 
 @Composable
 fun TodoListApp(
@@ -29,7 +74,22 @@ fun TodoListApp(
 ) {
     val currentUiState by viewModel.uiState.collectAsState()
 
-    Scaffold { innerPadding ->
+    Scaffold(
+        topBar = {
+            TodoListTopAppBar(
+                canNavigateHome = navController.previousBackStackEntry != null
+                        && navController.currentDestination?.route != TodoListPath.START,
+                navigateHome = {
+                    navController.popBackStack(
+                        route = TodoListPath.START,
+                        inclusive = false
+                    )
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
         NavHost(
             navController = navController,
             startDestination = TodoListPath.START,
