de298f8bb9869a1cebe432f65f70af4589a7746e
Dudoladov Dmitry <dudmitry.iv@gmail.com>
2025-09-11 17:42:37 +0300

    fix: create bg to save button

--- app/src/main/java/com/example/todolist/ui/ChangeTodoScreen.kt
+++ app/src/main/java/com/example/todolist/ui/ChangeTodoScreen.kt
@@ -1,9 +1,12 @@
 package com.example.todolist.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -11,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -42,11 +46,15 @@ fun ChangeTodoScreen(
             ),
             shape = MaterialTheme.shapes.large
         )
-        OutlinedButton(
+        OutlinedButton (
             onClick = { onTodoTitleSaveClick(todoId) },
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .padding(dimensionResource(R.dimen.padding_extraLarge))
+                .padding(dimensionResource(R.dimen.padding_extraLarge)),
+            colors = ButtonDefaults.outlinedButtonColors(
+                containerColor = MaterialTheme.colorScheme.surface
+            )
+
         ) {
             Text(
                 text = stringResource(R.string.save_btn),
