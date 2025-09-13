b063e93eac4c1e262e2eaae366ea8a8818eb27fc
Dudoladov Dmitry <dudmitry.iv@gmail.com>
2025-09-13 11:22:31 +0300

    feat: add UI state for dialogs

--- app/src/main/java/com/example/todolist/data/TodoListUiState.kt
+++ app/src/main/java/com/example/todolist/data/TodoListUiState.kt
@@ -6,5 +6,6 @@ data class TodoListUiState(
     val todoList: Map<String, Todo>,
     val changedTitle: String = "",
     val navigateToTask: String? = null,
-    val showFilterDialog: Boolean = false
+    val showFilterDialog: Boolean = false,
+    val showSearchDialog: Boolean = false,
 )
